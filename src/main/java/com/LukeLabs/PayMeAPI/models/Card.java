788a6caa216cf4420b22649e4c52b7a57a3429be
Luke Grayland <lgrayland96@gmail.com>
2025-05-07 21:12:13 +0100

    Override of .toString() method on Card object for custom log message

--- src/main/java/com/LukeLabs/PayMeAPI/models/Card.java
+++ src/main/java/com/LukeLabs/PayMeAPI/models/Card.java
@@ -23,4 +23,52 @@ public class Card {
     private String status;
     private String label;
     private int authCountLimit;
+
+    @Override
+    public String toString() {
+        return String.format("ID: %s " +
+                "Card Number: %s " +
+                "CVV: %s " +
+                "Expiry Month: %d " +
+                "Expiry Year: %d " +
+                "Limit: %.2f " +
+                "User ID: %d " +
+                "Start Date: %s " +
+                "End Date: %s " +
+                "Status: %s " +
+                "Label: %s " +
+                "Auth Count Limit: %d",
+                ID.toString(),
+                ObfuscatedSensitiveValue(cardNumber),
+                ObfuscatedSensitiveValue(cvv),
+                expiryMonth,
+                expiryYear,
+                limit,
+                userID,
+                startDate,
+                endDate,
+                status,
+                label,
+                authCountLimit
+        );
+    }
+
+    private static String ObfuscatedSensitiveValue(String value) {
+        if(value == null || value.isEmpty()) return value;
+
+        var length = value.length();
+        if(length < 3) return value;
+
+        if(length == 3) {
+            //Mask CVV
+            return String.valueOf('*').repeat(3);
+        }
+
+        if(length == 15 || length == 16) {
+            //Mask PAN
+            return String.valueOf('*').repeat(length - 4) + value.substring(length - 4, length);
+        }
+
+        return value;
+    }
 }
